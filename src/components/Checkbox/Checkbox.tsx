a73a9fd590b686e9ad74cfd10dfa1c191c733237
John Doe <johndoe@example.com>
2020-12-31 10:59:29 +0300

    add bold to checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Field } from 'redux-form';
 import './Checkbox.scss';
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { IFormStates } from '../../interface';
 
 type CheckboxProps = {
@@ -9,9 +9,12 @@ type CheckboxProps = {
 }
 
 const Checkbox: React.FC<CheckboxProps> = ({checked = false}) => {
+  const leftLabelClass = checked ? 'checkbox__left-label checkbox__label' : 'checkbox__left-label checkbox__label checkbox__label--active';
+  const rightLabelClass = checked ? 'checkbox__right-label checkbox__label checkbox__label--active' : 'checkbox__right-label checkbox__label';
+
   return (
     <div className="checkbox">
-      <span className="chekcbox__left-label chekcbox__label">Указать с НДФЛ</span>
+      <span className={leftLabelClass}>Указать с НДФЛ</span>
       <div className="form-check form-switch">
         <Field
           name="ndfl"
@@ -22,7 +25,7 @@ const Checkbox: React.FC<CheckboxProps> = ({checked = false}) => {
           className="form-check-input checkbox__control"
         />
       </div>
-      <span className="chekcbox__right-label chekcbox__label">Без НДФЛ</span>
+      <span className={rightLabelClass}>Без НДФЛ</span>
     </div>
   )
 }
